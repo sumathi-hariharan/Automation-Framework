2e7920f62dc124d8e1dc8e61d8f470b46347ca7b
sumathi <sumathi.hariharan@icloud.com>
2016-10-11 00:40:03 -0700

    commit2

---
+++ Documents/workspace/javaTest/src/arrays/ArrayObj.java
@@ -0,0 +1,23 @@
+package arrays;
+
+/* To print array with object */
+
+public class ArrayObj {
+ public static void printArray( int[] anArray){
+	 StringBuilder sb = new StringBuilder ();
+	 for (int i=0 ; i< anArray.length ; i++) {
+		if (i>0)  {
+			sb.append(",  ");
+		}
+			sb.append(anArray[i]);
+		}
+	 System.out.println(sb. toString());
+	 }
+ 
+	public static void main(String[] args) {
+		int [] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		printArray(testArray);
+
+	}
+
+}
